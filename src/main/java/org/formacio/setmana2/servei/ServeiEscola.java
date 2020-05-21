6aa02b44cb87b63c46aa3eece9791afc6c0b63d6
carloshoyos98 <choyos@cifpfbmoll.eu>
2020-05-21 16:36:20 +0200

    feat: Implementar rollback al producirse excepcion EdatIncorrecteException

--- src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
+++ src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(rollbackFor = EdatIncorrecteException.class)
 public class ServeiEscola {
 
 	@PersistenceContext
